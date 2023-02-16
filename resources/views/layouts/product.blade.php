2878e3003f690a4930d0bc7954b4f94f3e1309c4
unknown <alehhunko@gmail.com>
2023-02-16 23:06:11 +0300

    2-3

--- resources/views/layouts/product.blade.php
+++ resources/views/layouts/product.blade.php
@@ -1,8 +1,7 @@
 <div class="col-md-4 mt-5">
     <div class="card h-100">
         <!-- Product image-->
-        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
-            alt="..." />
+        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
         <!-- Product details-->
         <div class="card-body p-4">
             <div class="text-center">
@@ -15,10 +14,10 @@
         <!-- Product actions-->
         <div class="card-footer p-4 pt-0 border-top-0 bg-transparent ">
             <div class="d-flex justify-content-center">
-                <button type="button" class="btn btn-outline-primary mr-2">Options</button>
-                <button type="button" class="btn btn-outline-dark">
+                <a class="btn btn-outline-primary mt-auto mr-2" href="/shoes/shoes">Options</a>
+                <a class="btn btn-outline-dark mt-auto" href="/shopping_cart">
                     <span id="boot-icon" class="bi bi-cart"></span>
-                </button>
+                </a>
             </div>
         </div>
     </div>
